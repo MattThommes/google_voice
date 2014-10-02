911bdceab8ae814abbdf75b552e714b47bb8eace
Matt Thommes <matt@activecampaign.com>
2014-10-02 17:28:37 -0500

    adding initial files

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+	require "vendor/autoload.php";
+	use MattThommes\Debug;
+	$debug = new Debug;
+
+	$path_to_calls = "Calls";
+
+	function file_basename($path) {
+		$path = basename($path);
+		return (strlen($path) > 0 && substr($path, 0, 1) != '.' ? $path : '') ;
+	}
+
+	$files = array();
+	if ($handle = opendir($path_to_calls)) {
+		while (false !== ($file = readdir($handle))) {
+			$file = file_basename($file);
+			if ($file) {
+$debug->dbg($file);
+			}
+		}
+		closedir($handle);
+	}
+
+?>
