8770927360474ee2e911fc9eae4565eb4a498b27
Matt Thommes <matt@activecampaign.com>
2014-10-15 17:21:42 -0500

    building this out more to read the text message files

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 	$debug = new Debug;
 	include "db_connect.php";
 
-	$path_to_calls = "Calls";
+	// the path to where you have the Calls folder.
+	$path_to_calls = "/vagrant/dev/admin/google_voice";
 
 	function file_basename($path) {
 		$path = basename($path);
@@ -13,11 +14,24 @@
 	}
 
 	$files = array();
-	if ($handle = opendir($path_to_calls)) {
+	if ($handle = opendir("Calls")) {
 		while (false !== ($file = readdir($handle))) {
 			$file = file_basename($file);
 			if ($file) {
-$debug->dbg($file);
+				if (preg_match("- Text -", $file)) {
+$debug->dbg($file,1);
+					$parts = explode(" - Text - ", $file);
+					$from = $parts[0];
+					$time = trim($parts[1], ".html");
+//$debug->dbg($time,1);
+					$time = date("Y-m-d H:i:s", strtotime($time));
+//$debug->dbg($time);
+					$file_contents = file_get_contents($path_to_calls . "/Calls/" . $file);
+					$fd = FluentDOM($file_contents);
+					$message = $fd->find('//q')->text();
+$debug->dbg($message);
+$debug->dbg("testing just one");
+				}
 			}
 		}
 		closedir($handle);
